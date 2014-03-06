5546cfed147023fd8b5a7ade1e923046820c5a7a
Molly Trombley-McCann and Shatarupa Nandi <pair+molly+snandi@pivotallabs.com>
2014-03-06 10:25:04 -0800

    Removing caching from faces_fetcher because it was breaking tests.

--- lib/faces_fetcher.rb
+++ lib/faces_fetcher.rb
@@ -6,7 +6,7 @@ class FacesFetcher
   end
 
   def new_pivots(location)
-    @new_pivots ||= all_pivots_by_location(location).sort do |p1, p2|
+    @new_pivots = all_pivots_by_location(location).sort do |p1, p2|
       p2['started_on'] <=> p1['started_on']
     end[0..9].map do |pivot|
       {
@@ -19,7 +19,7 @@ class FacesFetcher
   end
 
   def all_pivots(location)
-    @all_pivots ||= all_pivots_by_location(location).map do |pivot|
+    @all_pivots = all_pivots_by_location(location).map do |pivot|
       {
           first_name: pivot['first_name'],
           last_name: pivot['last_name'],
@@ -30,7 +30,7 @@ class FacesFetcher
   end
 
   def all_pivots_by_location(location)
-    @all_pivots_by_location ||= @pivots.select do |pivot|
+    @all_pivots_by_location = @pivots.select do |pivot|
       pivot['location_name'] == location
     end
   end
