a95f6df15c95565ab49c881a838f6bf1017b12cc
Brian Kelly and Jacques Chester <pair+bkelly+jchester@pivotallabs.com>
2014-05-20 17:29:59 -0400

    Reduce CitiBike radius to prevent widget from overflowing

--- jobs/citibikenyc.rb
+++ jobs/citibikenyc.rb
@@ -6,8 +6,8 @@ latitude = ENV['NYC_LATITUDE'] || 40.740673
 longitude = ENV['NYC_LONGITUDE'] || -73.994808
 lat_long = [latitude, longitude]
 
-SCHEDULER.every '1m' do
-  stations = CitibikeFetcher.find(lat_long, 0.30)
+SCHEDULER.every '1m', first_at: Time.now do
+  stations = CitibikeFetcher.find(lat_long, 0.28)
 
   bike_counts = Hash.new({ value: 0 })
   stations.each do |station|
