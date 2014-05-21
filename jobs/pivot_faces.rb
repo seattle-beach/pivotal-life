16da3b13bd83c8c1a8b1ff45ce36b6409f9e291b
Brian Kelly and Jacques Chester <pair+bkelly+jchester@pivotallabs.com>
2014-05-21 17:19:26 -0400

    Pivots Faces job is now supports multiple offices.

--- jobs/pivot_faces.rb
+++ jobs/pivot_faces.rb
@@ -1,12 +1,18 @@
 require 'open-uri'
 
+offices_yaml = File.read('offices.yml')
+offices = Offices.load(offices_yaml)
+
 SCHEDULER.every '1d', first_in: 0 do
   pivots_user_email = ENV['PIVOTS_EMAIL'] || 'PIVOTS_USER_EMAIL_MISSING'
   pivots_auth_token = ENV['PIVOTS_AUTH_TOKEN'] || 'PIVOTS_AUTH_TOKEN_MISSING'
   pivots_datasource = "https://pivotalpivots2.herokuapp.com/api/users.json?email=#{URI::encode(pivots_user_email)}&authentication_token=#{URI::encode(pivots_auth_token)}"
 
-  send_event('pivot_faces', {
-      new_faces: NewFacesFetcher.new(pivots_datasource)['San Francisco'],
-      random_faces: RandomFacesFetcher.new(pivots_datasource)['San Francisco']
-  })
+  offices.each do |office|
+    send_event("pivot_faces_#{office.code}", {
+      new_faces: NewFacesFetcher.new(pivots_datasource)[office.config["name"]],
+      random_faces: RandomFacesFetcher.new(pivots_datasource)[office.config["name"]]
+    })
+  end
+
 end
