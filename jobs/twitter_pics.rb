c7a4e6f4796e9892fa73b85922e97180a10f178d
Mark McDonald <pair+mmcdonald@pivotallabs.com>
2014-08-29 16:40:18 -0400

    Capitalize default hashtag value

--- jobs/twitter_pics.rb
+++ jobs/twitter_pics.rb
@@ -6,7 +6,7 @@ Dotenv.load
 
 SCHEDULER.every '10m', :first_in => 0 do |job|
   begin
-    hashtag = ENV['TWITTER_PICTURE_HASHTAG'] || "pivotallife"
+    hashtag = ENV['TWITTER_PICTURE_HASHTAG'] || "PivotalLife"
     fetcher = PivotTwitterPictureFetcher.new(PivotTwitterNameFetcher.new(DefaultCredentials), TwitterPictureFetcher.new(DefaultClient))
     tweets = fetcher.get_pivot_picture_urls_by_hashtag(hashtag)
 
