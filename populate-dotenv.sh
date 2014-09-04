0c929e79397d0503e1176eade77e7797c85afb68
David Morhovich <pair+dmorhovich@pivotallabs.com>
2014-09-04 16:19:20 -0400

    Modify populate-dotenv to not include User-Provided=

--- populate-dotenv.sh
+++ populate-dotenv.sh
@@ -5,5 +5,5 @@ then
   echo "Not overwriting your .env file."
 else
   echo "Creating new .env file from Cloud Foundry pivotal-life environment"
-  CF_COLOR=false cf env pivotal-life | grep ':' | sed -e 's/\: */=/' > .env
+  CF_COLOR=false cf env pivotal-life | grep ':' | sed -e 's/\: */=/' | tail -n +2 > .env
 fi
