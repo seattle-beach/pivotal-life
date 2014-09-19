d8016d54dbfdd91d157413e88d9c4ebee3867945
NathanS/RebeccaP <rputinski@pivotal.io>
2014-09-18 21:10:34 -0400

    Add Toronto to Pivotal life - Start a widget for Toronto lunch places -- it'll be awesome   - adds yelp

---
+++ jobs/lunch_place.rb
@@ -0,0 +1,20 @@
+require 'dotenv'
+require 'yelp'
+
+Dotenv.load
+
+yelp_client = Yelp::Client.new({
+  consumer_key: ENV['YELP_CONSUMER_KEY'],
+  consumer_secret: ENV['YELP_CONSUMER_SECRET'],
+  token: ENV['YELP_AUTH_TOKEN'],
+  token_secret: ENV['YELP_AUTH_SECRET']
+})
+
+places = YAML.load(File.read('to_lunchplaces.yml'))['places']
+
+SCHEDULER.every '30s', first_in: 0 do
+  current_place = places.sample
+  data = { place: current_place }
+  data['rating'] = "#{yelp_client.business(current_place['yelp_biz']).rating}/5"
+  send_event('lunch_places_to', data)
+end
