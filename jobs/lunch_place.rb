6e8a4dfd2716c7d771c189e2f1661567c7862635
NathanS/RebeccaP <rputinski@pivotal.io>
2014-09-18 22:03:09 -0400

    Add more lunch spots

--- jobs/lunch_place.rb
+++ jobs/lunch_place.rb
@@ -15,6 +15,15 @@ places = YAML.load(File.read('to_lunchplaces.yml'))['places']
 SCHEDULER.every '30s', first_in: 0 do
   current_place = places.sample
   data = { place: current_place }
-  data['rating'] = "#{yelp_client.business(current_place['yelp_biz']).rating}/5"
+  data[:rating] = "#{yelp_client.business(current_place['yelp_biz']).rating}/5"
+
+  if current_place['distance'] < 3
+    data['wittyremark'] = "Come on, it's soo close!"
+  elsif current_place['distance'] < 8
+    data['wittyremark'] = "Pretty close... maybe not in the winter."
+  else
+    data['wittyremark'] = "Don't miss your 1pm meeting!"
+  end
+
   send_event('lunch_places_to', data)
 end
