4c1629b5816c62f9e829e90d0ff20ce2ae61391c
Brian Kelly and Paul Meskers <pair+bkelly+pmeskers@pivotallabs.com>
2014-09-22 10:11:41 -0400

    Slow down forecast fetcher service to every 15m
    We appear to be hitting our rate limit with forecast.io, so as an easy
    fix we are just going to increase the time between scheduled jobs.

--- jobs/forecast.rb
+++ jobs/forecast.rb
@@ -7,7 +7,7 @@ Dotenv.load
 # Forecast API Key from https://developer.forecast.io
 forecast_api_key = ENV['FORECAST_API_KEY']
 
-SCHEDULER.every '5m', :first_in => 0 do |job|
+SCHEDULER.every '15m', :first_in => 0 do |job|
   location_forecasts = ForecastFetcher.new(forecast_api_key).data
   location_forecasts.each_pair do |location, data|
     send_event "forecast-#{location}", data
