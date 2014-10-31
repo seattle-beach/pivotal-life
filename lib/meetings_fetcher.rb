6b536df44898bdb19d163e7ead6ddeb8df304daa
Gabelan Satchithanantham <gsatchithanantham@pivotal.io>
2014-10-31 14:24:08 -0400

    Updated to use ENV variables and Redesigned dashboard

--- lib/meetings_fetcher.rb
+++ lib/meetings_fetcher.rb
@@ -3,9 +3,6 @@ require 'google/api_client/client_secrets'
 require 'google/api_client/auth/installed_app'
 
 #TODO: Request id and secret from a different account
-CLIENT_ID = '551647463843-ujkp9ouirdpk34e25mpe43i749piprq5.apps.googleusercontent.com'
-CLIENT_SECRET = 'rK5_AGwSfnnST3LDMn5-Aw6E'
-REFRESH_TOKEN = '1/SLD1by-eSeTslM9vDB0Bt0xTCFASPj7aSl6LixmUi0I'
 SCOPE = ['https://www.googleapis.com/auth/calendar']
 
 class MeetingsFetcher
@@ -16,15 +13,9 @@ class MeetingsFetcher
       application_version: '1.0.0'
     )
 
-<<<<<<< HEAD
-    @client.authorization.refresh_token=REFRESH_TOKEN
-    @client.authorization.client_id=CLIENT_ID
-    @client.authorization.client_secret=CLIENT_SECRET
-=======
     @client.authorization.client_id=ENV['GOOGLE_CLIENT_ID']
     @client.authorization.client_secret=ENV['GOOGLE_CLIENT_SECRET']
     @client.authorization.refresh_token=ENV['GOOGLE_REFRESH_TOKEN']
->>>>>>> 8c26b41... Added Meeting Rooms Dashboard + Widget
 
     @calendar = @client.discovered_api('calendar', 'v3')
   end
@@ -46,17 +37,9 @@ class MeetingsFetcher
       parameters: {
         calendarId: calendar_id,
         timeMax: (Time.now + (60*60*24)).utc.iso8601,
-<<<<<<< HEAD
-        timeMin: Time.now.utc.iso8601
-=======
-<<<<<<< HEAD
         timeMin: Time.now.utc.iso8601,
         singleEvents: true,
         orderBy: "startTime"
-=======
-        timeMin: Time.now.utc.iso8601
->>>>>>> Added Meeting Rooms Dashboard + Widget
->>>>>>> 8c26b41... Added Meeting Rooms Dashboard + Widget
       },
       headers: {'Content-Type' => 'application/json'}
     })
@@ -71,15 +54,7 @@ class MeetingsFetcher
       temp = {
         title: event['visibility']=='private' ? 'Private' : event['summary'],
         location: event['location'].nil? ? '' : event['location'],
-<<<<<<< HEAD
-        organizer: event['organizer'].nil? ? '' : event['organizer']['displayName'],
-=======
-<<<<<<< HEAD
         organizer: event['organizer'].nil? ? 'Anonymous' : event['organizer']['displayName'],
-=======
-        organizer: event['organizer'].nil? ? '' : event['organizer']['displayName'],
->>>>>>> Added Meeting Rooms Dashboard + Widget
->>>>>>> 8c26b41... Added Meeting Rooms Dashboard + Widget
         organizer_email: event['organizer'].nil? ? '' : event['organizer']['email'],
         start: Time.parse(event['start']['dateTime']).to_i,
         end: Time.parse(event['end']['dateTime']).to_i,
@@ -94,22 +69,10 @@ class MeetingsFetcher
   end
 
   def get_room_status(events)
-<<<<<<< HEAD
-    green =  { text: 'Available', style: 'green' }
-    yellow = { text: 'Reserved', style: 'yellow' }
-    red =    { text: 'In Use', style: 'red' }
-=======
-<<<<<<< HEAD
     green =  { text: 'Available - Room is not Booked', style: 'green' }
     yellow = { text: 'Reserved - A Meeting will Start Soon', style: 'yellow' }
     red =    { text: 'In Use - Room is Booked', style: 'red' }
 
-=======
-    green =  { text: 'Available', style: 'green' }
-    yellow = { text: 'Reserved', style: 'yellow' }
-    red =    { text: 'In Use', style: 'red' }
->>>>>>> Added Meeting Rooms Dashboard + Widget
->>>>>>> 8c26b41... Added Meeting Rooms Dashboard + Widget
     status = green
     current_time = Time.now.to_i
     warning_time = 5*60
