e250ce2e9623e65238b91419c529ae811de455bb
Brian Kelly <polymonic@gmail.com>
2014-11-16 14:25:46 -0500

    Updates and re-adds Movember job/widget to NYC dashboard.

--- spec/lib/movember_fetcher_spec.rb
+++ spec/lib/movember_fetcher_spec.rb
@@ -5,14 +5,14 @@ require_relative '../../lib/movember_fetcher'
 describe MovemberFetcher do
   describe "#fetch" do
     let(:page_source) { File.open('spec/fixtures/movember_page_source.html') }
-    let(:fetcher) { MovemberFetcher.new(1005517, page_source) }
+    let(:fetcher) { MovemberFetcher.new(1712277, page_source) }
 
     it "returns the correct team name" do
-      expect(fetcher.fetch[:label]).to eq "Pivotal - NYC"
+      expect(fetcher.fetch[:label]).to eq "Pivotal - New York City"
     end
 
     it "returns the correct team donation total" do
-      expect(fetcher.fetch[:value]).to eq "$4,764"
+      expect(fetcher.fetch[:value]).to eq "$3,024"
     end
   end
 end
