e458874f73bc16482b69069737de191f340af393
Joseph Rodriguez <jrodriguez@pivotal.io>
2015-06-26 10:11:55 -0600

    Fix forecast specs for denver addition

--- spec/lib/forecast_fetcher_spec.rb
+++ spec/lib/forecast_fetcher_spec.rb
@@ -31,6 +31,10 @@ describe ForecastFetcher do
     'https://api.forecast.io/forecast/FAKE-API-KEY/40.01499,-105.27055?units=us'
   end
 
+  let(:denver_forecast_url) do
+    'https://api.forecast.io/forecast/FAKE-API-KEY/39.7392,-104.9903?units=us'
+  end
+
   before do
     stub_request(:get, nyc_forecast_url).to_return(:body => File.read('spec/fixtures/forecast/nyc.json'))
     stub_request(:get, pa_forecast_url).to_return(:body => File.read('spec/fixtures/forecast/palo-alto.json'))
@@ -38,6 +42,7 @@ describe ForecastFetcher do
     stub_request(:get, london_forecast_url).to_return(:body => File.read('spec/fixtures/forecast/london.json'))
     stub_request(:get, toronto_forecast_url).to_return(:body => File.read('spec/fixtures/forecast/toronto.json'))
     stub_request(:get, boulder_forecast_url).to_return(:body => File.read('spec/fixtures/forecast/boulder.json'))
+    stub_request(:get, denver_forecast_url).to_return(:body => File.read('spec/fixtures/forecast/denver.json'))
   end
 
   it 'collects forecast data for each location' do
@@ -92,15 +97,25 @@ describe ForecastFetcher do
       :next_desc=>"Mostly cloudy for the hour.",
       :next_icon=>"partly-cloudy-day"
     })
-    expect(forecast_fetcher.data[:to]).to eq({
-      :current_temp=>"13°C",
-      :current_icon=>"partly-cloudy-day",
-      :current_desc=>"Mostly Cloudy",
-      :apparent_temp=>"13°C",
-      :later_desc=>"Partly cloudy later this evening.",
-      :later_icon=>"partly-cloudy-day",
-      :next_desc=>"Mostly cloudy for the hour.",
-      :next_icon=>"partly-cloudy-day"
+    expect(forecast_fetcher.data[:boulder]).to eq({
+        :current_temp=>"13°F",
+        :apparent_temp=>"13°F",
+        :current_icon=>"partly-cloudy-day",
+        :current_desc=>"Mostly Cloudy",
+        :later_desc=>"Partly cloudy later this evening.",
+        :later_icon=>"partly-cloudy-day",
+        :next_desc=>"Mostly cloudy for the hour.",
+        :next_icon=>"partly-cloudy-day"
+    })
+    expect(forecast_fetcher.data[:denver]).to eq({
+        :current_temp=>"13°F",
+        :apparent_temp=>"13°F",
+        :current_icon=>"partly-cloudy-day",
+        :current_desc=>"Mostly Cloudy",
+        :later_desc=>"Partly cloudy later this evening.",
+        :later_icon=>"partly-cloudy-day",
+        :next_desc=>"Mostly cloudy for the hour.",
+        :next_icon=>"partly-cloudy-day"
     })
   end
 end
