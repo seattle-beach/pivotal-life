270b706f270e79a376860e952b8b5d70e84c2e1f
aaravin <aaravin@ncsu.edu>
2015-11-10 12:00:21 -0500

    Fix populate-dotenv.sh so that it does not show VCAP_APPLICATION or User_Provided attributes

--- populate-dotenv.sh
+++ populate-dotenv.sh
@@ -5,5 +5,5 @@ then
   echo "Not overwriting your .env file."
 else
   echo "Creating new .env file from Cloud Foundry pivotal-life environment"
-  CF_COLOR=false cf env pivotal-life | grep ':' | sed -e 's/\: */=/' | tail -n +2 > .env
+  CF_COLOR=false cf env pivotal-life | sed '/Getting env variables.*/,/User-Provided:/d' | grep ':' | sed -e 's/\: */=/' | tail -n +2 > .env
 fi
