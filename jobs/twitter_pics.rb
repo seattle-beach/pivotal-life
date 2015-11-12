6955b9f504d67c410ee7ba5731c7ce2565a1267c
Ashwin Aravindan and Danny Joyce <pair+aaravindan+djoyce@pivotal.io>
2015-11-12 15:31:38 -0500

    Twitter picture job always calls send_event even if the twitter api call failed.

--- jobs/twitter_pics.rb
+++ jobs/twitter_pics.rb
@@ -5,15 +5,14 @@ require 'dotenv'
 Dotenv.load
 
 SCHEDULER.every '10m', :first_in => 0 do |job|
+  tweets = []
   begin
     hashtag = ENV['TWITTER_PICTURE_HASHTAG'] || "PivotalLife"
     fetcher = PivotTwitterPictureFetcher.new(PivotTwitterNameFetcher.new(DefaultCredentials), TwitterPictureFetcher.new(DefaultClient))
     tweets = fetcher.get_pivot_picture_urls_by_hashtag(hashtag)
-
-    if tweets
-      send_event('twitter_pictures', tweets: tweets)
-    end
   rescue Twitter::Error
     puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
+  ensure
+    send_event('twitter_pictures', tweets: tweets)
   end
 end
