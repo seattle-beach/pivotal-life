8378a5ea9efb05940e7725a0108a3a30d130210c
Ashwin Aravindan and Danny Joyce <pair+aaravindan+djoyce@pivotal.io>
2015-11-12 16:51:29 -0500

    Sort offices by name in office_links job.

--- jobs/office_links.rb
+++ jobs/office_links.rb
@@ -7,5 +7,7 @@ conf.each do |key, array|
   offices.push({'tag' => key, 'name' => array['name']})
 end
 
+offices.sort! {|x, y| x['name'] <=> y['name']}
+
 send_event('office_links', { office_links: offices })
 
