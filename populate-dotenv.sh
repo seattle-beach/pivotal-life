2715d82adf7b3e326316409cef7f372a15b07e39
George Gelashvili <ggelashvili@pivotal.io>
2016-01-08 18:21:58 -0500

    gave populate-dotenv more functionality.
    It can now append or exclusively use custom variables in varlist to the .env
    Necessary for persisting any desired changes to the environment variables for pivotal-life

--- populate-dotenv.sh
+++ populate-dotenv.sh
@@ -1,9 +1,41 @@
 #!/bin/sh
 
-if [ -e .env ]
+ENV_FILE=".env"
+VAR_LIST="varlist"
+
+createFromCF() {
+  echo "Creating new .env file from Cloud Foundry pivotal-life environment."
+  CF_COLOR=false cf env pivotal-life | sed '/Getting env variables.*/,/User-Provided:/d' | grep ':' | sed -e 's/\: */=/' > $ENV_FILE
+}
+
+addFromVarlist() {
+  echo "Using variables from varlist."
+  cat $VAR_LIST >> $ENV_FILE
+}
+
+
+if [ -e $ENV_FILE ]
 then
   echo "Not overwriting your .env file."
 else
-  echo "Creating new .env file from Cloud Foundry pivotal-life environment"
-  CF_COLOR=false cf env pivotal-life | sed '/Getting env variables.*/,/User-Provided:/d' | grep ':' | sed -e 's/\: */=/' > .env
+  if [ -e $VAR_LIST ]
+  then
+    echo "varlist found. From which should I create the .env file? [default: 1]"
+    echo
+    echo "1 - from the CF pivotal-life environment, then append varlist"
+    echo "2 - from varlist, ignoring variables in the CF pivotal-life environment"
+    echo "3 - from the CF pivotal-life environment, ignoring varlist"
+    echo
+    read sel
+    case $sel in
+      "") createFromCF; addFromVarlist;;
+      1) createFromCF; addFromVarlist;;
+      2) addFromVarlist;;
+      3) createFromCF;;
+      *) echo "Bad value. Nothing was done, so you should re-run this script if you want an .env";;
+    esac
+  else
+    echo "No varlist."
+    createFromCF
+  fi
 fi
